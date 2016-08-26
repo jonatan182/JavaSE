8adbad7ffeb1d0738c6ce31be03593c0d98aa67a
jonatan182 <xxcinder_182@hotmail.com>
2016-08-26 18:33:04 -0500

    JSE

---
+++ Graficas/src/graficas.java
@@ -0,0 +1,13 @@
+
+public class graficas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interface obj = new Interface();
+        obj.setLocationRelativeTo(null);
+        obj.setTitle("Metodos Numericos");
+        obj.setVisible(true);
+    }
+}
